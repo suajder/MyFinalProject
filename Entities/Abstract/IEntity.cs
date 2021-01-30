4c4ccdbe9a4887ad13e5adb943608e37b04302e1
suajder <sajder@ogu.edu.tr>
2021-01-31 00:20:17 +0200

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Ientity implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
